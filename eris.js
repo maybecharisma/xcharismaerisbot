07e928a268d4c81bb6b72ce6f228c4923763cdb6
maybecharisma <36387595+maybecharisma@users.noreply.github.com>
2018-03-15 23:43:31 +0200

    Update eris.js

--- eris.js
+++ eris.js
@@ -29,12 +29,41 @@ setInterval(function(){
             }
 dark.editChannel("423952721952112642" , {name : "📅 Date " + "[" + day + "-" + month + "-" + years + "]"})
 }, 3000);
-dark.editChannel("423957110947184641", {name : "🕐 Time   [" + hours + ":" + minutes  +" " + suffix + "]"}) 
-}, 3000);
-dark.editChannel("423954648764907521", {name : "- Bot-Users  [" + dark.users.size + "]"})
+
+dark.editChannel("423954648764907521", {name : "- Bot-Users  [" + dark.users.size + "]"});
 }, 3000);
  
+});
 
+dark.on("ready", ready => {
+setInterval(function(){
+ 
+            var currentTime = new Date(),
+            hours = currentTime.getHours() + 2 ,
+            minutes = currentTime.getMinutes(),
+            seconds = currentTime.getSeconds(),
+            years = currentTime.getFullYear(),
+            month = currentTime.getMonth() + 1,
+            day = currentTime.getDate(),
+            week = currentTime.getDay();
+           
+             
+ 
+            if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+            var suffix = "AM";
+            if (hours >= 12) {
+                suffix = "PM";
+                hours = hours - 12;
+            }
+            if (hours == 0) {
+                hours = 12;
+            }
+dark.editChannel("423957110947184641", {name : "🕐 Time   [" + hours + ":" + minutes  +" " + suffix + "]"}) 
+}, 3000);
+ 
+});
 
 
 dark.connect("set_token")
