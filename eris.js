598a7280d038f60098d5e43eae84eee796cc6489
maybecharisma <36387595+maybecharisma@users.noreply.github.com>
2018-03-15 23:22:35 +0200

    Update eris.js

--- eris.js
+++ eris.js
@@ -27,7 +27,7 @@ setInterval(function(){
             if (hours == 0) {
                 hours = 12;
             }
-dark.editChannel('419939569358602243' ,'423952721952112642' , {name : "📅 Date " + "[" + day + "-" + month + "-" + years + "]"})
+dark.editChannel("423952721952112642" , {name : "📅 Date " + "[" + day + "-" + month + "-" + years + "]"})
 }, 3000);
  
 });
