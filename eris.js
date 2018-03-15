79de8bebc9cc5f2bddbea049494fb946ae7d32d2
maybecharisma <36387595+maybecharisma@users.noreply.github.com>
2018-03-16 00:04:23 +0200

    Update eris.js

--- eris.js
+++ eris.js
@@ -5,7 +5,7 @@ var dark = new Eris("Mzk5NDU2MDY1NTAwMDg2Mjgw.DYxzKQ.ahOx3r9vxTRP5Zka16i9vJvlls0
 dark.on("ready", ready => {
 setInterval(function(){
  
-           const currentTime = new Date(),
+           var currentTime = new Date(),
             hours = currentTime.getHours() + 2 ,
             minutes = currentTime.getMinutes(),
             seconds = currentTime.getSeconds(),
@@ -27,15 +27,40 @@ setInterval(function(){
             if (hours == 0) {
                 hours = 12;
             }
- dark.editChannel("423952721952112642" , {name : "📅 Date " + "[" + day + "-" + month + "-" + years + "]"})
-}, 3000);
-
+ 
  
 dark.editChannel("423957110947184641", {name : "🕐 Time   [" + hours + ":" + minutes  +" " + suffix + "]"}) 
 }, 3000);
  
 
 
+    var currentTime = new Date(),
+            hours = currentTime.getHours() + 2 ,
+            minutes = currentTime.getMinutes(),
+            seconds = currentTime.getSeconds(),
+            years = currentTime.getFullYear(),
+            month = currentTime.getMonth() + 1,
+            day = currentTime.getDate(),
+            week = currentTime.getDay();
+           
+             
+ 
+            if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+            var suffix = "AM";
+            if (hours >= 12) {
+                suffix = "PM";
+                hours = hours - 12;
+            }
+            if (hours == 0) {
+                hours = 12;
+            }
+ 
+ dark.editChannel("423952721952112642" , {name : "📅 Date " + "[" + day + "-" + month + "-" + years + "]"})
+}, 3000);
+
+ 
 
 
 
